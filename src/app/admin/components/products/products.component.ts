1bb5e03619dcaa2bd4d084236e90f19d5aceb7dc
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2024-05-29 17:22:13 +0300

    Custom HttpClient Service

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { Product } from 'src/app/contracts/product';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
   selector: 'app-products',
@@ -8,11 +10,91 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
-  constructor(spinner: NgxSpinnerService) {
+  constructor(
+    spinner: NgxSpinnerService,
+    private httpClientService: HttpClientService
+  ) {
     super(spinner);
   }
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallSpinClockwiseFadeRotating);
+
+    console.log(5);
+
+    this.httpClientService
+      .get<Product[]>({
+        // fullEndpoint: "https/sdhsdksdk"
+        controller: 'products',
+      })
+      .subscribe((data) => console.log(data));
+
+    // this.httpClientService
+    //   .post(
+    //     {
+    //       controller: 'products',
+    //     },
+    //     {
+    //       name: 'Kalem',
+    //       stock: 100,
+    //       price: 15,
+    //     }
+    //   )
+    //   .subscribe();
+
+    // this.httpClientService
+    //   .post(
+    //     {
+    //       controller: 'products',
+    //     },
+    //     {
+    //       name: 'Kağıt',
+    //       stock: 50,
+    //       price: 2.5,
+    //     }
+    //   )
+    //   .subscribe();
+
+    // this.httpClientService
+    //   .post(
+    //     {
+    //       controller: 'products',
+    //     },
+    //     {
+    //       name: 'Silgi',
+    //       stock: 100,
+    //       price: 12,
+    //     }
+    //   )
+    //   .subscribe();
+
+    // this.httpClientService
+    //   .put(
+    //     {
+    //       controller: 'products',
+    //     },
+    //     {
+    //       id: '43c328fb-02a0-43d8-90df-42395442ce56',
+    //       name: 'Renkli Kağıt',
+    //       stock: 250,
+    //       price: 5.5,
+    //     }
+    //   )
+    //   .subscribe();
+
+    // this.httpClientService
+    //   .delete(
+    //     {
+    //       controller: 'products',
+    //     },
+    //     '37a49e2d-0f46-49a0-b738-fa190674879b'
+    //   )
+    //   .subscribe();
+
+    this.httpClientService
+      .get({
+        fullEndpoint: 'https://jsonplaceholder.typicode.com/posts',
+      })
+      .subscribe((data) => console.log(data));
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,9 @@ export class AppComponent {
   constructor() {}
 }
 
-$.get('https://localhost:7208/api/products', (data) => {
-  console.log(data);
-});
+// $.get('https://localhost:7208/api/products', (data) => {
+//   console.log(data);
+// });
 
 // $(document).ready(() => {
 //   alert('asd');
