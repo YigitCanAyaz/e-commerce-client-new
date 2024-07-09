8301ab79b2799d6cb60a0e977207f3125ec239af
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2024-07-09 22:56:03 +0300

    Authorization

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -3,12 +3,21 @@ import { HttpClientService } from '../http-client.service';
 import { Create_User } from 'src/app/contracts/users/create_user';
 import { User } from 'src/app/entities/user';
 import { firstValueFrom, Observable } from 'rxjs';
+import {
+  CustomToastrService,
+  ToastrMessageType,
+  ToastrPosition,
+} from '../../ui/custom-toastr.service';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private httpClientService: HttpClientService) {}
+  constructor(
+    private httpClientService: HttpClientService,
+    private toastrService: CustomToastrService
+  ) {}
 
   async create(user: User): Promise<Create_User> {
     const observable: Observable<Create_User | User> =
@@ -26,16 +35,33 @@ export class UserService {
     userNameOrEmail: string,
     password: string,
     callBackFunction?: () => void
-  ): Promise<void> {
-    const observable: Observable<any> = this.httpClientService.post(
-      {
-        controller: 'users',
-        action: 'login',
-      },
-      { userNameOrEmail, password }
-    );
+  ): Promise<any> {
+    const observable: Observable<any | TokenResponse> =
+      this.httpClientService.post<any | TokenResponse>(
+        {
+          controller: 'users',
+          action: 'login',
+        },
+        { userNameOrEmail, password }
+      );
 
-    await firstValueFrom(observable);
+    const tokenResponse: TokenResponse = (await firstValueFrom(
+      observable
+    )) as TokenResponse;
+
+    if (tokenResponse) {
+      localStorage.setItem('accessToken', tokenResponse.token.accessToken);
+
+      this.toastrService.message(
+        'Kullanıcı girişi başarıyla sağlanmıştır.',
+        'Giriş Başarılı',
+        {
+          messageType: ToastrMessageType.Success,
+          position: ToastrPosition.TopRight,
+        }
+      );
+    }
+
     callBackFunction();
   }
 }
